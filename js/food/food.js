d32f15f8bcde83c3cd2fc026185e9ebd7ce5d978
Matthew Powell <matt.powell2784@gmail.com>
2023-07-20 15:57:43 +0100

    add fodo images and logic

--- js/food/food.js
+++ js/food/food.js
@@ -1,3 +1,5 @@
+import { foodImageList } from './foodImageList.js'
+
 export let foodOnBoard = false
 export let foodPosition = null
 
@@ -5,10 +7,24 @@ export const setFoodOnBoard = (value) => {
   foodOnBoard = value
 }
 
+let nextFoodImage = 0
+
+const getNextFoodImage = () => {
+  const nextImage = foodImageList[nextFoodImage]
+  nextFoodImage++
+  if (nextFoodImage >= foodImageList.length) nextFoodImage = 0
+  return nextImage
+}
+
 export const addFood = (gameBoard) => {
   const randomCoordinates = getRandomBoardPosition()
   foodPosition = randomCoordinates
   const foodElement = document.createElement('div')
+  const image = document.createElement('img')
+  const nextFoodImage = getNextFoodImage()
+  console.log('nextFoodImage', nextFoodImage)
+  image.src = `./assets/food/${nextFoodImage}`
+  foodElement.appendChild(image)
 
   foodElement.style.gridColumnStart = randomCoordinates.x
   foodElement.style.gridRowStart = randomCoordinates.y
---
+++ js/food/foodImageList.js
@@ -0,0 +1,18 @@
+export const foodImageList = [
+  // 'carlos.jpg',
+  // 'deepa.jpg',
+  'dylan.jpg',
+  'elena.jpg',
+  'eun.jpg',
+  'george.jpg',
+  'georgina.jpg',
+  'izzy.jpg',
+  'james.jpg',
+  'laurie.jpg',
+  'matt.jpg',
+  'nick.jpg',
+  'shaughn.jpg',
+  'tess.jpg',
+  'tomaso.jpg',
+  'yuqing.jpg',
+]
